b601e113798e2f27787729d69383ee9e55f29222
Miles Oberstadt <miles@oberstadt.com>
2020-04-15 22:56:58 -0500

    Fix string compare and clean up

--- src/twitter-watch.ts
+++ src/twitter-watch.ts
@@ -1,20 +1,15 @@
 const dotenv = require('dotenv');
 const fs = require('fs');
-const { promisify } = require('util');
-const exec = promisify(require ('child_process').exec);
 import { fetchFile, getVideoLength, lengthenVideo } from './util/video.util';
 import { TwitterUtil } from './util/twitter.util';
-import { copyFile } from 'fs';
 
 dotenv.config();
 
 const loopMinLength = 300; // Minimum length of video in seconds
 const videoProcessingDir = './tmp'; // Temp dir for processing videos and tracking 
-const watchedVideoDir = '/home/pi/videos'; // Dir that linux is watching for new videos to play
 
 try {
     fs.mkdirSync(videoProcessingDir);
-    fs.mkdirSync(watchedVideoDir);
 }
 catch(e) {
     // we don't care if the dir already exists
@@ -28,7 +23,7 @@ async function main() {
     // tweet.id_str
     try{
         const lastShown = fs.readFileSync(`${videoProcessingDir}/latest`);
-        if (lastShown === tweet.id_str){
+        if (lastShown.toString() === tweet.id_str){
             console.error("We already have the latest, exiting...")
             return;
         }
@@ -41,18 +36,15 @@ async function main() {
         throw new Error('Video only for now, sorry');
     const origVidPath = `${videoProcessingDir}/current_short.mp4`;
     const longerVidPath = `${videoProcessingDir}/current.mp4`;
-    const finalVidPath = `${watchedVideoDir}/current.mp4`;
     await fetchFile(media.url, origVidPath);
 
     const videoLength = await getVideoLength(origVidPath);
     if (videoLength < loopMinLength){
         await lengthenVideo(videoLength, loopMinLength, videoProcessingDir);
-        await fs.promises.copyFile(longerVidPath, finalVidPath);
     }
     else {
-        await fs.promises.copyFile(origVidPath, finalVidPath);
+        await fs.promises.copyFile(origVidPath, longerVidPath);
     }
-    await fs.writeFileSync(`${videoProcessingDir}/latest`, tweet.id_str);
     console.log('DONE');
 }
 
